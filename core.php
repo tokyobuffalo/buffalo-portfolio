9c4ddabf289743ea57a827d6942e8982b5ae5d20
Kevin O'Neil <kevin@hivelocity.co.jp>
2014-11-05 01:20:52 +0900

    added plugin header

---
+++ core.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * Plugin Name: Buffalo Photography Portfolio 
+ * Description: A plugin that you can use to show your photy
+ * Version: 0.0.1
+ * Author: Tokyo Buffalo
+ * Author URI: http://www.github.com/tokyobuffalo
+ * License: GPL2
+ */
+
+ //silence is golden
