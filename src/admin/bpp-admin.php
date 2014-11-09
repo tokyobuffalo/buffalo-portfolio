d9726401070f0189fbc58ae7118732e2dc411d8a
Kevin O'Neil <kevin@hivelocity.co.jp>
2014-11-09 17:31:21 +0900

    fixed admin checkbox error (affected every check box)

--- src/admin/bpp-admin.php
+++ src/admin/bpp-admin.php
@@ -84,7 +84,7 @@ class Plugin_Name_Admin {
 		 * class.
 		 */
 
-		wp_enqueue_style( $this->plugin_name, plugin_dir_url( __FILE__ ) . 'css/plugin-name-admin.css', array(), $this->version, 'all' );
+		wp_enqueue_style( $this->plugin_name, plugin_dir_url( __FILE__ ) . 'css/bpp-admin.css', array(), $this->version, 'all' );
 
 	}
 
@@ -107,7 +107,7 @@ class Plugin_Name_Admin {
 		 * class.
 		 */
 
-		wp_enqueue_script( $this->plugin_name, plugin_dir_url( __FILE__ ) . 'js/plugin-name-admin.js', array( 'jquery' ), $this->version, false );
+		wp_enqueue_script( $this->plugin_name, plugin_dir_url( __FILE__ ) . 'js/bpp-admin.js', array( 'jquery' ), $this->version, false );
 
 	}
 
--- src/admin/buffalo-options.php
+++ src/admin/buffalo-options.php
@@ -14,24 +14,26 @@ function bpp_admin_html() { ?>
 		</thead>
 		<tbody>
 			<tr>
-				<th>Images</th>
-				<td class="table-center"><input type="checkbox"></td>
-				<td class="table-center"><input type="checkbox"></td>
+				<th scope="row" class="bold"><label for="bpp-display-all">Images</label>
+				<p>Control whether or not social media buttons will appear over images</p>
+				</th>
+				<td class="table-center vertical-middle bpp-input"><input type="checkbox" id="bpp-display-all" class="bpp-input"></td>
+				<td class="table-center vertical-middle bpp-input"><input type="checkbox"></td>
 			</tr>
 			<tr class="alternate">
 				<th>Facebook</th>
-				<td class="table-center"><input type="checkbox"></td>
-				<td class="table-center"><input type="checkbox"></td>
+				<td class="table-center bpp-input"><input type="checkbox"></td>
+				<td class="table-center bpp-input"><input type="checkbox"></td>
 			</tr>
 			<tr>
 				<th>Twitter</th>
-				<td class="table-center"><input type="checkbox"></td>
-				<td class="table-center"><input type="checkbox"></td>
+				<td class="table-center bpp-input"><input type="checkbox" class="bpp-input"></td>
+				<td class="table-center bpp-input"><input type="checkbox" class="bpp-input"></td>
 			</tr>
 			<tr class="alternate">
 				<th>LinkedIn</th>
-				<td class="table-center"><input type="checkbox"></td>
-				<td class="table-center"><input type="checkbox"></td>
+				<td class="table-center bpp-input"><input type="checkbox"></td>
+				<td class="table-center bpp-input"><input type="checkbox"></td>
 			</tr>
 		</tbody>
 	</table>
